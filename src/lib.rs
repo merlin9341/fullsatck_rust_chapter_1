c95ba24951e0c564e3650d88e18d0f58cc37f7a3
merlin9341 <felixmlroughton@gmail.com>
2024-03-18 14:54:07 +0000

    added new funciton generate_sequence to produce list of numbers based on arguemnt for print()

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,20 @@ pub fn say_hello() {
     println!("Hello, world!");
 }
 
-pub fn print() {
-    let vector_numbers = vec![1, 2, 3, 4, 5];
-    output_sequence(&vector_numbers);
-    let array_numbers =  [1, 2, 3, 4, 5];
-    output_sequence(&array_numbers);
-
+pub fn print(limit: u8) {
+    let numbers = generate_sequence(limit);
+    output_sequence(&numbers)
 }
-pub fn output_sequence(numbers: &[u8]) {
-    for n in numbers.iter() {
+fn generate_sequence(limit: u8) -> Vec<u8> {
+    let mut numbers = Vec::new();
+    for n in 1..=limit {
+        numbers.push(n);
+    }
+    numbers
+}
+
+fn output_sequence(numbers: &[u8]) {
+    for n in numbers {
         println!("{}", n);
     }
-
 }
