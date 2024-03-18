eff8de4bed6b4caf5d9a1ed2f7ea0ced8de28976
merlin9341 <felixmlroughton@gmail.com>
2024-03-18 14:18:22 +0000

    Chaged to using slices of both a vector and an array

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,13 @@ pub fn say_hello() {
 }
 
 pub fn print() {
-    let numbers = [1, 2, 3, 4, 5];
-    output_sequence(numbers);
+    let vector_numbers = vec![1, 2, 3, 4, 5];
+    output_sequence(&vector_numbers);
+    let array_numbers =  [1, 2, 3, 4, 5];
+    output_sequence(&array_numbers);
+
 }
-pub fn output_sequence(numbers: [u8; 5]) {
+pub fn output_sequence(numbers: &[u8]) {
     for n in numbers.iter() {
         println!("{}", n);
     }
